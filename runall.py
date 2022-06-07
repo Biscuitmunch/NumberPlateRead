ef2dc058c8d6ee2b0822a79dab0d61cd26963ab9
Biscuitmunch <biscuitmunch0@gmail.com>
2022-06-07 19:12:55 +1200

    Changed runall and image 8

--- runall.py
+++ runall.py
@@ -2,22 +2,31 @@ import os
 from threading import Thread
 
 def task1():
-    os.system("python3 CS373LicensePlateDetection.py numberplate1.png")
+    os.system("python CS373LicensePlateDetection.py numberplate1.png")
 
 def task2():
-    os.system("python3 CS373LicensePlateDetection.py numberplate2.png")
+    os.system("python CS373LicensePlateDetection.py numberplate2.png")
 
 def task3():
-    os.system("python3 CS373LicensePlateDetection.py numberplate3.png")
+    os.system("python CS373LicensePlateDetection.py numberplate3.png")
 
 def task4():
-    os.system("python3 CS373LicensePlateDetection.py numberplate4.png")
+    os.system("python CS373LicensePlateDetection.py numberplate4.png")
 
 def task5():
-    os.system("python3 CS373LicensePlateDetection.py numberplate5.png")
+    os.system("python CS373LicensePlateDetection.py numberplate5.png")
 
 def task6():
-    os.system("python3 CS373LicensePlateDetection.py numberplate6.png")
+    os.system("python CS373LicensePlateDetection.py numberplate6.png")
+
+def task7():
+    os.system("python CS373LicensePlateDetection.py numberplate7.png")
+
+def task8():
+    os.system("python CS373LicensePlateDetection.py numberplate8.png")
+
+def task9():
+    os.system("python CS373LicensePlateDetection.py numberplate9.png") 
 
 t1 = Thread(target=task1)
 t2 = Thread(target=task2)
@@ -25,6 +34,9 @@ t3 = Thread(target=task3)
 t4 = Thread(target=task4)
 t5 = Thread(target=task5)
 t6 = Thread(target=task6)
+t7 = Thread(target=task7)
+t8 = Thread(target=task8)
+t9 = Thread(target=task9)
 
 t1.start()
 t2.start()
@@ -32,5 +44,8 @@ t3.start()
 t4.start()
 t5.start()
 t6.start()
+t7.start()
+t8.start()
+t9.start()
 
 # 6 and 1.5 good ratios
